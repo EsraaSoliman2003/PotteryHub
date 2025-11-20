aa209a74f6ff54191653daa12c43ee321307c1ee
Esraa Soliman <ee341110@gmail.com>
2025-11-20 22:07:44 +0200

    run back

--- BackEnd/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbbf67727debc79a513fc49866eef334d56e7d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe91a4f29b8c14c27ad1a45ded82583f0a400e06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
